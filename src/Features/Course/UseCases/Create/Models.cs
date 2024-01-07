a0137edbc141ecd6751fd18c582a4fc23272ca5b
MateusSantosF <mateusferreira.dev@gmail.com>
2024-01-07 00:54:53 -0300

    feat(Course): add create endpoint

---
+++ src/Features/Course/UseCases/Create/Models.cs
@@ -0,0 +1,38 @@
+
+
+namespace timeasy_api.src.Modules.course.UseCases.Create;
+public class Request
+{
+    public string Name { get; set; }
+    public int PeriodAmount { get; set; }
+    public string Turn { get; set; }
+    public string Period { get; set; }
+
+    [FromClaim("InstituteId", isRequired: true)]
+    public Guid InstituteId { get; set; }
+
+}
+public class Validator : Validator<Request>
+{
+    public Validator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("O parametro Name é obrigatório.")
+            .MinimumLength(5).WithMessage("Informe seu nome completo.");
+
+
+        RuleFor(x => x.PeriodAmount)
+            .GreaterThan(0)
+            .WithMessage("A quantidade de Periodos deve ser maior que zero.");
+    }
+}
+
+public class Response
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int PeriodAmount { get; set; }
+    public string Turn { get; set; }
+    public string Period { get; set; }
+    public Guid InstituteId { get; set; }
+}
