87b05f4afc61ffc2f71b95c0da676c588086689b
MateusSantosF <mateusferreira.dev@gmail.com>
2024-01-07 00:53:54 -0300

    feat(PagedRequest): add validations

--- src/Core/Pagination/PagedRequest.cs
+++ src/Core/Pagination/PagedRequest.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace timeasy_api.src.Core.Pagination;
 
 
 public abstract class PagedRequest
 {
+
+    [Range(1, int.MaxValue, ErrorMessage = "O valor de 'PageSize' deve ser maior ou igual a 1.")]
     public int PageSize { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "O valor de 'Page' deve ser maior ou igual a 1.")]
     public int Page { get; set; }
 }
